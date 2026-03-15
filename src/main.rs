1578b5f29e476f452c97efc6a88985e527c59fc6
Ivan Sim <ihcsim@gmail.com>
2026-03-15 15:28:53 -0700

    discovery and retrieve virt-handler pods
    Signed-off-by: Ivan Sim <ihcsim@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,53 @@ use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 
+use k8s_openapi::api::core::v1::Pod;
+use kube::{
+    Client,
+    api::{Api, ListParams},
+};
+
 mod cpu_caps;
 mod de;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let ns = "kubevirt";
+    let selector = "kubevirt.io=virt-handler";
+
+    let k8s_client = Client::try_default().await?;
+    let pods: Api<Pod> = Api::namespaced(k8s_client, ns);
+    let list_params = ListParams::default().labels(selector);
+    for pod in pods.list(&list_params).await? {
+        let status = match pod.status {
+            Some(status) => status,
+            None => continue,
+        };
+        if let Some(phase) = status.phase
+            && phase != "Running"
+        {
+            continue;
+        }
+
+        let pod_name = match pod.metadata.name {
+            Some(name) => name,
+            None => continue,
+        };
+
+        let spec = match pod.spec {
+            Some(spec) => spec,
+            None => continue,
+        };
+        let node_name = match spec.node_name {
+            Some(node_name) => node_name,
+            None => continue,
+        };
+        println!(
+            "Found virt-handler pod: {} on node: {}",
+            pod_name, node_name
+        );
+    }
+
     let path = Path::new("testdata").join("virsh_domcapabilities.xml");
     let xml_file = File::open(path)?;
     let buf = BufReader::new(xml_file);
@@ -40,7 +83,6 @@ Using API: QEMU 11.0.0
         virt_launcher_version,
     );
 
-    let output = cpu_caps.to_yaml()?;
-    println!("{}", output);
+    let _output = cpu_caps.to_yaml()?;
     Ok(())
 }
